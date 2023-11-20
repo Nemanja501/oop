9b9724d20c61a975b647ffff3638b09ca0ba0f43
Nemanja501 <nemanjavulin5@gmail.com>
2023-11-20 21:28:10 +0100

    prosirio vezbu 1

--- dan1/vezba1.php
+++ dan1/vezba1.php
@@ -2,49 +2,67 @@
 class BankAccount{
     public $currentBalance = 0;
     public bool $isBlocked = false;
+    protected $lowerLimit = -200;
+    protected $provizija = 0;
 
     public function checkIfAccountIsBlocked(){
-        if($this->currentBalance <= -200){
+        if($this->currentBalance <= $this->lowerLimit){
             $this->isBlocked = true;
             if($this->currentBalance >= 0){
                 $this->isBlocked = false;
             }
         }
     }
-}
-
-class User{
-    public string $firstName;
-    public string $lastName;
-    public BankAccount $account;
-
-    public function __construct($firstName, $lastName)
-    {
-        $this->firstName = $firstName;
-        $this->lastName = $lastName;
-        $this->account = new BankAccount();
-    }
 
     public function withdraw($amount){
-        if($this->account->isBlocked == false){
-            $this->account->currentBalance -= $amount;
-            $this->account->checkIfAccountIsBlocked();
+        if($this->isBlocked == false){
+            $this->currentBalance -= $amount + ($amount * ($this->provizija / 100));
+            $this->checkIfAccountIsBlocked();
         }else{
             echo "Your account is blocked. \n";
         }
     }
 
     public function deposit($amount){
-        $this->account->currentBalance += $amount;
-        $this->account->checkIfAccountIsBlocked();
+        $this->currentBalance += $amount - ($amount * ($this->provizija / 100));
+        $this->checkIfAccountIsBlocked();
+    }
+
+    public function balance(){
+        echo "Your current account balance is: " . $this->currentBalance . "\n";
+    }
+
+}
+
+class SimpleBankAccount extends BankAccount{
+}
+
+class SecuredBankAccount extends BankAccount{
+    protected $lowerLimit = -1000;
+    protected $provizija = 2.5;
+}
+
+class User{
+    public string $firstName;
+    public string $lastName;
+    public SimpleBankAccount $simpleAccount;
+    public SecuredBankAccount $securedAccount;
+
+    public function __construct($firstName, $lastName)
+    {
+        $this->firstName = $firstName;
+        $this->lastName = $lastName;
+        $this->simpleAccount = new SimpleBankAccount();
+        $this->securedAccount = new SecuredBankAccount();
     }
 }
 
 $user = new User('Marko', 'Markovic');
-$user->deposit(400);
-echo $user->account->currentBalance . "\n";
-$user->withdraw(700);
-echo $user->account->currentBalance . "\n";
-$user->withdraw(100);
-$user->deposit(500);
-echo $user->account->currentBalance . "\n";
+$user->securedAccount->withdraw(200);
+$user->securedAccount->balance();
+$user->simpleAccount->withdraw(200);
+$user->simpleAccount->balance();
+
+$user->securedAccount->withdraw(100);
+$user->simpleAccount->withdraw(100);
+
