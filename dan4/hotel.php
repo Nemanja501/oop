504d667bd77b38571617ab8de4154098ea71ad1b
Nemanja501 <nemanjavulin5@gmail.com>
2023-11-27 19:28:07 +0100

    hotel vezba2

--- dan4/hotel.php
+++ dan4/hotel.php
@@ -59,9 +59,67 @@ class TrokrevetnaSobaFactory implements SobaFactory{
     }
 }
 
+class Hotel{
+    private static $instance;
+    private $listaSoba = [
+        JednokrevetnaSoba::class => [],
+        DvokrevetnaSoba::class => [],
+        TrokrevetnaSoba::class => []
+    ];
+
+    private $slobodneSobe = [
+        JednokrevetnaSoba::class => 0,
+        DvokrevetnaSoba::class => 0,
+        TrokrevetnaSoba::class => 0
+    ];
+
+    private function __construct()
+    {
+        
+    }
+
+    static function getInstance(){
+        if(self::$instance == null){
+            self::$instance = new Hotel();
+        }
+        return self::$instance;
+    }
+
+    public function addSoba(Soba $soba){
+        $tipSobe = get_class($soba);
+        $this->listaSoba[$tipSobe][] = $soba;
+        $this->slobodneSobe[$tipSobe]++;
+    }
+
+    public function iznajmiSobu(string $tipSobe){
+        if($this->slobodneSobe[$tipSobe] > 0){
+            echo "Iznajmljena " . $tipSobe . "\n";
+            $this->slobodneSobe[$tipSobe]--;
+            return $this->listaSoba[$tipSobe][array_rand($this->listaSoba[$tipSobe])];
+        }else{
+            echo "Nema slobodnih soba \n";
+        }
+    }
+
+}
+
 $jednokrevetnaSobaFactory = new JednokrevetnaSobaFactory();
+$dvokrevetnaSobaFactory = new DvokrevetnaSobaFactory();
 $jednokrevetnaSoba = $jednokrevetnaSobaFactory->createSoba();
 $jednokrevetnaSoba->setBrojSobe(55);
 $jednokrevetnaSoba->setPrivatnoKupatilo(true);
 $jednokrevetnaSoba->setImaBalkon(false);
-var_dump($jednokrevetnaSoba);
+Hotel::getInstance()->addSoba($jednokrevetnaSoba);
+$jednokrevetnaSoba2 = $jednokrevetnaSobaFactory->createSoba();
+$jednokrevetnaSoba2->setBrojSobe(10);
+$jednokrevetnaSoba2->setPrivatnoKupatilo(false);
+$jednokrevetnaSoba2->setImaBalkon(false);
+Hotel::getInstance()->addSoba($jednokrevetnaSoba2);
+$dvokrevetnaSoba = $dvokrevetnaSobaFactory->createSoba();
+$dvokrevetnaSoba->setBrojSobe(77);
+$dvokrevetnaSoba->setPrivatnoKupatilo(true);
+$dvokrevetnaSoba->setImaBalkon(true);
+Hotel::getInstance()->addSoba($dvokrevetnaSoba);
+
+$iznajmljenaSoba = Hotel::getInstance()->iznajmiSobu(DvokrevetnaSoba::class);
+var_dump($iznajmljenaSoba);
